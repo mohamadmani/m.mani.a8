b0a37489660dcc63121c7ec0c0b968c9fba6de88
mohamadmani <149293821+mohamadmani@users.noreply.github.com>
2023-12-08 22:37:32 +0330

    Add files via upload

---
+++ hmw4.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+  float x,y,z,p,s;
+     
+cout<<"please enter your first number:";
+cin>>x;
+
+cout<<" please enter your second number:";
+cin>>y;
+
+    z=x+y; 
+	
+     cout<<z<<"\n ";
+	
+	p=x-y;
+	
+	cout<<p<<"\n ";
+	
+	s=x*y;
+	
+	cout<<s<<"\n";
+	
+	
+}
