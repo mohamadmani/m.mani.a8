89a875b67438f7687b0203e4c69ff3924a28feee
mohamadmani <149293821+mohamadmani@users.noreply.github.com>
2023-12-22 23:06:34 +0330

    Add files via upload

---
+++ hm4.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+   using namespace std;
+
+int main() {
+	
+int x,y,z,s,m,h,d,i,q;
+
+	cout<<"please enter your frist  number:";
+	cin>>x;
+	
+	cout<<"please enter your second number:";
+	cin>>y;
+	
+	cout<<"please enter your third number:";
+	cin>>z;
+	
+	s=x-y;
+	
+	q=x+z;
+	
+	i=s*q;
+	
+	m=(x*x)+(x*y)+(x*z)+(y*x)+(y*y)+(y*z)+(z*x)+(z*y)+(z+z);
+	
+	h=m+1;
+	
+	d=s/h;
+	 
+	 cout<<d;
+	
+
+	
+	
+	
+}
